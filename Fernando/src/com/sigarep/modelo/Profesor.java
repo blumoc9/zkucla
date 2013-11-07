65dae556f0d5654012b7ee76c84ba2fdc35e1072
blumoc9 <blumxp@gmail.com>
2013-11-07 07:47:39 -0430

    comentarios en parte del codigo

--- Fernando/src/com/sigarep/modelo/Profesor.java
+++ Fernando/src/com/sigarep/modelo/Profesor.java
@@ -4,6 +4,13 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+/* El @Entity anotación indica que el JavaBean es una entidad persistente. 
+ * APP conteste automáticamente a esta categoría, excepto la configuración JPA 
+ * registra explícitamente las clases de cada unidad de persistencia debe manejar. 
+ * Una @table  anotación puede configurar explícitamente qué tabla la entidad se asigna a,
+ *  aunque en este caso no es necesario ya que por defecto el nombre de la clase que coincide 
+ *  con el nombre de la tabla.
+ *  http://es.wikipedia.org/wiki/JavaBean */
 @Entity
 @Table(name = "profesor")
 public class Profesor {
